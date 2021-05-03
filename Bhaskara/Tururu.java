18dae0cec438a7b0fe8673cea9832654ce0fe51b
Fernando Andrade <onanduandrade@gmail.com>
2021-05-03 11:41:06 -0300

    Update Tururu.java

--- Bhaskara/Tururu.java
+++ Bhaskara/Tururu.java
@@ -9,13 +9,46 @@ package tururu;
  *
  * @author fernando
  */
-public class Tururu {
+import java.util.Arrays;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Tururu {
+    
     public static void main(String[] args) {
-        System.out.println("sadsadsa");
+
+        int coeficienteA = Integer.parseInt(args[0]);
+
+        int coeficienteB = Integer.parseInt(args[1]);
+
+        int coeficienteC = Integer.parseInt(args[2]);
+
+        int deltaDiscritante = discritante(coeficienteA, coeficienteB, coeficienteC);
+
+        int[] resutlado = (deltaDiscritante >= 0) ? calcularRaizes(deltaDiscritante, coeficienteA, coeficienteB) : resultadoNegativo(deltaDiscritante);
+
+        System.out.println(Arrays.toString(resutlado));
+        System.out.println(deltaDiscritante);
+    }
+
+    public static int discritante(int a, double b, int c) {
+        int coeficienteB  = (int) Math.pow(b, 2);
+
+        int delta = coeficienteB - 4 * a * c;
+
+        return delta;
     }
     
+    public static int[] calcularRaizes(int delta, int a, int b) {
+        int xPositiva = (-(b) + ((int)Math.sqrt(delta))) / (2 * a);
+
+        int xNegativa = (-(b) - ((int)Math.sqrt(delta))) / (2 * a);
+
+        return new int[] { xPositiva, xNegativa};
+    }
+
+    public static int[] resultadoNegativo (int delta) {
+
+        int valorDeltaNegativo = 0;
+        
+        return new int[] { valorDeltaNegativo };
+    }
 }
