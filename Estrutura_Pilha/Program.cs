b343a2ece3096caacf3ee25003aa5a8ef3c75d11
fernanduandrade <onanduandrade@gmail.com>
2021-05-20 23:02:14 -0300

    Adicionar estrutude de pilha

---
+++ Estrutura_Pilha/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Estrutura_Pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pilha = new Pilha();
+            pilha.Empilhar(1);
+            pilha.Empilhar(10);
+            pilha.Empilhar(100);
+
+            Console.WriteLine(pilha.Desempilhar());
+            Console.WriteLine(pilha.Desempilhar());
+            Console.WriteLine(pilha.Desempilhar());
+            Console.WriteLine(pilha.Desempilhar());
+        }
+    }
+}
