a717bf12850bc706df7b6856947e3343f70bb9e6
fernanduandrade <onanduandrade@gmail.com>
2021-06-01 22:00:52 -0300

    add AreaDoCirculo

---
+++ AreaDoCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaDoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            Console.WriteLine("Digite o valor do raio: ");
+
+            double raio = double.Parse(Console.ReadLine());
+            double raioAoQuadrado = Math.Pow(raio, 2);
+            double resultado = pi * raioAoQuadrado;
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
