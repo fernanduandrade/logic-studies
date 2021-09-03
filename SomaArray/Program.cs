db797be31031611dee713bddddb71ac198f552f5
fernanduandrade <onanduandrade@gmail.com>
2021-09-03 08:22:29 -0300

    Adicionado exercico para somar array

---
+++ SomaArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace SomaArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5,6 };
+            int resultado = array.Sum();
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
