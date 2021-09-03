49e274d9f503880691c38d4493152cec1b02dd29
fernanduandrade <onanduandrade@gmail.com>
2021-09-03 08:43:55 -0300

    Adicionado exercicio para encontrar maior elemento da lista

---
+++ MaiorValorEmArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System;
+
+namespace MaiorValorEmArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5,6,7,8,9};
+            int resultado = array.Max();
+
+            Console.WriteLine("O maior elemento da lista é " + resultado); // 9
+        }
+    }
+}
