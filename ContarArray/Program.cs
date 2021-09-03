a44299146e3a5f934ac9c04599114a3362b336cb
fernanduandrade <onanduandrade@gmail.com>
2021-09-03 08:27:55 -0300

    Adicionado exercicio para contar lista

---
+++ ContarArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ContarArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int resultado = array.Count();
+            
+            Console.WriteLine("A lista possui " + 6 + " elementos");
+        }
+    }
+}
