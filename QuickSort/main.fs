8cd35d39dfcbffd346329911689c0262a479325d
fernanduadnrade <onanduandrade@gmail.com>
2022-05-12 07:18:32 -0300

    feat: quicksort

---
+++ QuickSort/main.fs
@@ -0,0 +1,14 @@
+module QuickSort.Main
+
+
+
+let rec quicksort list =
+  match list with
+  | [] -> []
+  | x :: xs ->
+    let smaller = List.filter ((>) x) xs in
+    let bigger = List.filter ((<) x) xs in
+      quicksort smaller @ [x] @ quicksort bigger;
+
+printfn "%A" (quicksort [2;4;200;30;8;121;14;35;20;30])
+
